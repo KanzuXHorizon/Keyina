f7db9b3b2fdc0cb8bfa0b55ea634bc369a38d6a2
Shinryu Kanzu <KanzuWakazaki.Github@protonmail.com>
2026-07-30 22:30:58 +0700

    chore(release): bump version to 0.1.1

--- core/include/keyina/version.h
+++ core/include/keyina/version.h
@@ -4,6 +4,6 @@ namespace keyina {
 
 inline constexpr int kVersionMajor = 0;
 inline constexpr int kVersionMinor = 1;
-inline constexpr int kVersionPatch = 0;
+inline constexpr int kVersionPatch = 1;
 
 }  // namespace keyina
